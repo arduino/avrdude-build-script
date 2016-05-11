dfc7dad900ea39d7926c8976900ae714766d5136
Cristian Maglie <c.maglie@arduino.cc>
2016-05-11 12:21:13 +0200

    Added generic run-build.bash

---
+++ run-build.bash
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+OS=`uname -o || uname`
+
+if [[ $OS == "GNU/Linux" ]] ;
+then
+  MACHINE=`uname -m`
+  if [[ $MACHINE == "x86_64" ]] ; then
+    ./arch.linux64.build.bash
+    exit 0
+  fi
+
+  if [[ $MACHINE == "i686" ]] ; then
+    ./arch.linux32.build.bash
+    exit 0
+  fi
+  echo Linux Machine not supported: $MACHINE
+  exit 1
+fi
+
+if [[ $OS == "Msys" || $OS == "Cygwin" ]] ;
+then
+  ./arch.win32.build.bash
+  exit 0
+fi
+
+if [[ $OS == "Darwin" ]] ;
+then
+  ./arch.mac.build.bash
+  exit 0
+fi
+
+echo OS Not supported: $OS
+exit 2
+
