3841cc22c28f8b9e792d09369b2c7c18cc000297
Martino Facchin <m.facchin@arduino.cc>
2018-02-15 15:13:34 +0100

    Update version to arduino13

--- package-avrdude.bash
+++ package-avrdude.bash
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-OUTPUT_VERSION=6.3.0-arduino12
+OUTPUT_VERSION=6.3.0-arduino13
 
 export OS=`uname -o || uname`
 export TARGET_OS=$OS
