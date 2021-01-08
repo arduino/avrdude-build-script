6fb8410a43678724a94292d6dc8c321b4c0b1423
Martino Facchin <m.facchin@arduino.cc>
2021-01-08 11:20:35 +0100

    update version to arduino19

--- package-avrdude.bash
+++ package-avrdude.bash
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-OUTPUT_VERSION=6.3.0-arduino18
+OUTPUT_VERSION=6.3.0-arduino19
 
 export OS=`uname -o || uname`
 export TARGET_OS=$OS
